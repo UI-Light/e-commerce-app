8633587706ab68337580c81c7ff12e08f1b19f4c
Aonani <Lightkarlton@gmail.com>
2024-02-21 15:33:53 +0100

    complete product details page

--- lib/features/product_details/presentation/views/product_details_page.dart
+++ lib/features/product_details/presentation/views/product_details_page.dart
@@ -34,156 +34,191 @@ class _ProductDetailsPageState extends State<ProductDetailsPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              decoration: const BoxDecoration(
-                color: Palette.categoryGridBg,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(50),
-                  bottomRight: Radius.circular(50),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  color: Palette.categoryGridBg,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Icon(Icons.arrow_back_ios),
+                      Expanded(
+                        child: Center(
+                          child: Image.asset(
+                            'assets/perfume.png',
+                            height: MediaQuery.of(context).size.height / 2,
+                            width: MediaQuery.of(context).size.width / 2,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(18.0),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Icon(Icons.arrow_back_ios),
-                    Expanded(
-                      child: Center(
-                        child: Image.asset(
-                          'assets/perfume.png',
-                          height: MediaQuery.of(context).size.height / 2,
-                          width: MediaQuery.of(context).size.width / 2,
+                    const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Garnier White Skincare',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
+                        Stars(),
+                      ],
+                    ),
+                    const SizedBox(height: 4.0),
+                    const Text(
+                      '\$200.00',
+                      style: TextStyle(
+                          color: Palette.blue,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 25.0,
+                    ),
+                    const Text(
+                      'Select size',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          color: Palette.productGridBg,
+                          child: const Text('100 GM'),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          color: Palette.productGridBg,
+                          child: const Text('150 GM'),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Container(
+                          color: Palette.productGridBg,
+                          child: const Text('200 GM'),
+                        ),
+                        const SizedBox(
+                          width: 60,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            decrement();
+                          },
+                          child: const Text(
+                            '-',
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          counter.toString(),
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          width: 20,
+                          color: Palette.blue,
+                          child: GestureDetector(
+                              onTap: () {
+                                increment();
+                              },
+                              child: const Center(
+                                child: Text(
+                                  '+',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15.0),
+                    const Text(
+                      'Product Details',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    const Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+                      textAlign: TextAlign.justify,
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                            elevation: MaterialStatePropertyAll(0.0),
+                            backgroundColor:
+                                MaterialStatePropertyAll(Palette.blue),
+                            // fixedSize: MaterialStatePropertyAll<Size>(
+                            //   Size.fromWidth(160),
+                            // ),
+                            minimumSize:
+                                MaterialStatePropertyAll<Size>(Size(160, 40)),
+                          ),
+                          child: const Text('Add to cart'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: const ButtonStyle(
+                            elevation: MaterialStatePropertyAll(0.0),
+                            backgroundColor: MaterialStatePropertyAll(
+                                Palette.categoryGridBg),
+                            //fixedSize: MaterialStatePropertyAll<Size>(
+                            // Size.fromWidth(160),
+                            // ),
+                            minimumSize:
+                                MaterialStatePropertyAll<Size>(Size(160, 40)),
+                          ),
+                          child: const Text('Buy Now',
+                              style: TextStyle(
+                                color: Colors.black,
+                              )),
+                        ),
+                      ],
                     )
                   ],
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Garnier White Skincare',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Stars(),
-                    ],
-                  ),
-                  const SizedBox(height: 4.0),
-                  const Text(
-                    '\$200.00',
-                    style: TextStyle(
-                        color: Palette.blue,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  const SizedBox(
-                    height: 25.0,
-                  ),
-                  const Text(
-                    'Select size',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        color: Palette.productGridBg,
-                        child: const Text('100 GM'),
-                      ),
-                      Container(
-                        color: Palette.productGridBg,
-                        child: const Text('150 GM'),
-                      ),
-                      Container(
-                        color: Palette.productGridBg,
-                        child: const Text('200 GM'),
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            decrement();
-                          },
-                          child: const Text('-')),
-                      Text(counter.toString()),
-                      TextButton(
-                          onPressed: () {
-                            increment();
-                          },
-                          child: const Text('+')),
-                    ],
-                  ),
-                  const SizedBox(height: 15.0),
-                  const Text(
-                    'Product Details',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  const Text(
-                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
-                    textAlign: TextAlign.justify,
-                  ),
-                  const SizedBox(height: 15),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: const ButtonStyle(
-                          elevation: MaterialStatePropertyAll(0.0),
-                          backgroundColor:
-                              MaterialStatePropertyAll(Palette.blue),
-                          // fixedSize: MaterialStatePropertyAll<Size>(
-                          //   Size.fromWidth(160),
-                          // ),
-                          minimumSize:
-                              MaterialStatePropertyAll<Size>(Size(160, 40)),
-                        ),
-                        child: const Text('Add to cart'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        style: const ButtonStyle(
-                          elevation: MaterialStatePropertyAll(0.0),
-                          backgroundColor:
-                              MaterialStatePropertyAll(Palette.categoryGridBg),
-                          //fixedSize: MaterialStatePropertyAll<Size>(
-                          // Size.fromWidth(160),
-                          // ),
-                          minimumSize:
-                              MaterialStatePropertyAll<Size>(Size(160, 40)),
-                        ),
-                        child: const Text('Buy Now',
-                            style: TextStyle(
-                              color: Colors.black,
-                            )),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
