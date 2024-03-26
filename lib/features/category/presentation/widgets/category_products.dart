d99699e4662fdc4559e57eb7b3cece2d531c3d46
Crazelu <eberelucky64@gmail.com>
2024-03-26 11:23:35 +0100

    fix: performs search on products after loading all products

--- lib/features/category/presentation/widgets/category_products.dart
+++ lib/features/category/presentation/widgets/category_products.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/core/models/category_model.dart';
 import 'package:shopping_app/core/presentation/widgets/products_box.dart';
 import 'package:shopping_app/features/category/presentation/view_model/category_products_viewmodel.dart';
-
 import 'package:shopping_app/features/category/presentation/widgets/items_loading_listview.dart';
 
 class CategoryProducts extends StatefulWidget {
   final Category category;
   final ValueNotifier<String> searchQuery;
-  const CategoryProducts(
-      {super.key, required this.category, required this.searchQuery});
+  const CategoryProducts({
+    super.key,
+    required this.category,
+    required this.searchQuery,
+  });
 
   @override
   State<CategoryProducts> createState() => _CategoryProductsState();
@@ -25,14 +27,15 @@ class _CategoryProductsState extends State<CategoryProducts> {
 
   @override
   void initState() {
-    print('init state called');
-    categoryProductsViewModel.getProductsInCategory(widget.category);
-    widget.searchQuery.addListener(_searchQueryListener);
-    if (widget.searchQuery.value.isNotEmpty) {
-      print('something else');
-      categoryProductsViewModel.search(widget.searchQuery.value);
-    }
     super.initState();
+    categoryProductsViewModel.getProductsInCategory(widget.category).then(
+      (value) {
+        if (widget.searchQuery.value.isNotEmpty) {
+          categoryProductsViewModel.search(widget.searchQuery.value);
+        }
+      },
+    );
+    widget.searchQuery.addListener(_searchQueryListener);
   }
 
   @override
